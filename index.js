6a02ee7161f54312ea3a8805899933fbc76f7911
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-07-25 21:23:07 -0400

    made inquirer run infinitely

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const inquirer = require("inquirer");
-const fs = require('fs')
+const fs = require('fs');
+
 
 // Questions for database selection
 const questions = [{
     
         type: 'list',
         message: "Choose a license for your project.",
-        choices: ['View all departments', 'View all roles', 'View all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+        choices: ['View all departments', 'View all roles', 'View all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'Quit'],
         name: 'choice'
-    }
-]
+}]
 
 async function init() {
     const response = await inquirer.prompt(questions)
+
+    if(response.choice !== 'Quit') {
+        init()
+    } 
 }
 
 init()
